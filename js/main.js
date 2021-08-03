d6e1ec76e1bac8ed506017f2c9bbdf86d1e828ba
sahil-120 <87125805+sahil-120@users.noreply.github.com>
2021-08-03 11:08:28 +0545

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,24 @@
+// var video = document.getElementById("myVideo");
+// var btn = document.getElementById("myBtn");
+
+// function myFunction() {
+//   if (video.paused) {
+//     video.play();
+//     btn.innerHTML = "Pause";
+//   } else {
+//     video.pause();
+//     btn.innerHTML = "Play";
+//   }
+// }
+
+const signUpButton = document.getElementById('signUp');
+const signInButton = document.getElementById('signIn');
+const container = document.getElementById('container');
+
+signUpButton.addEventListener('click', () => {
+    container.classList.add("right-panel-active");
+});
+
+signInButton.addEventListener('click', () => {
+    container.classList.remove("right-panel-active");
+});
